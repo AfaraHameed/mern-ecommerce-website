5da7d921ca94a03f34b4aae1e15796be4b0cb287
Afara <afarasidheek@gmail.com>
2021-12-13 16:21:36 +0400

    react bootstrap5 installed

---
+++ admin-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css';
+import { Jumbotron } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+     <Jumbotron>
+       <h1>Admin dash board</h1>
+     </Jumbotron>
+    </div>
+  );
+}
+
+export default App;
